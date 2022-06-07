92dd357ad4c01e2708747d937320e51994645865
alejandromz <37418752+alejandromz@users.noreply.github.com>
2022-06-07 17:42:56 -0500

    Update i2s_audio.py

--- test code/i2s_audio.py
+++ test code/i2s_audio.py
@@ -7,15 +7,15 @@ bck_pin = Pin(22)   # Pin BCLK
 ws_pin = Pin(21)    # Pin LRC
 sdout_pin = Pin(23) # Pin Din
 
-audio_out = I2S(1,                                  # create I2S peripheral to write audio
-                sck=bck_pin, ws=ws_pin, sd=sdout_pin,   # sample data to an Adafruit I2S Amplifier
-                mode=I2S.TX,  # breakout board, 
-                bits=16,                        # based on MAX98357A device
+audio_out = I2S(1,
+                sck=bck_pin, ws=ws_pin, sd=sdout_pin,
+                mode=I2S.TX, 
+                bits=16, 
                 format=I2S.MONO,
                 rate=16000,
                 ibuf=2000)
                
-samples = bytearray([1,0,1,0,0,0,1,1,1,1,0,1,1,0]*100)                                 # bytearray containing audio samples to transmit
+samples = bytearray([1,0,1,0,0,0,1,1,1,1,0,1,1,0]*100)
 
 while True:
     num_bytes_written = audio_out.write(samples)
